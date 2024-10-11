d171d8f5e4fda1e1b68a1cd4f705a6583ce467ef
[email protected] <[email protected]>
2024-10-11 11:27:55 +0300

    add:object car

---
+++ object.ts
@@ -0,0 +1,8 @@
+const machineCharacteristic = {
+    salon: 'натуральнвя кожа красного цвета',
+    securitySistems: 'alarm',
+    cushions:'камера заднего вида',
+    exterior:'чёрные диски'
+}
+
+console.log(machineCharacteristic)
