8d4ca88bc06cacf6a2be941c05e2f7149ffe94f0
[email protected] <[email protected]>
2024-10-18 11:37:15 +0300

    add: array.ts

---
+++ array.ts
@@ -0,0 +1,8 @@
+const fruits = ['apple', 'banana', 'grape', 'watermelon', 'kiwi','orange' ]
+console. log (fruits.at (-1))
+const vegetables = ['cucumber','tomato','potatoes','cabbage','carrots']
+console. log (vegetables.at (-1))
+fruits.push('gorilla')
+const plants = fruits.concat(vegetables)
+plants.sort()
+console.log (plants)
