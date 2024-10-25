39049c53460ddd0b11109163a7e1715986ea1df6
[email protected] <[email protected]>
2024-10-25 11:41:33 +0300

    add:carsArray

---
+++ Forarray.ts
@@ -0,0 +1,11 @@
+const carsArray = ['Jaguar', 'McLaren', 'audit', 'BMW', 'toyota' ]
+
+for(let y=0; y < carsArray.length; y++){
+    if(carsArray[y]==='toyota'){
+        console.log(`в данном массиве нйден ${carsArray[y]}`)
+    }
+}
+
+
+carsArray[0]
+
