fdb09274fd2b947fb62115c75f710b4351c9c265
[email protected] <[email protected]>
2024-11-22 11:44:10 +0300

    add :sort array

---
+++ sort.js
@@ -0,0 +1,7 @@
+const words = ["socks","water","socket","grandmother","gas",];
+const resuilUp = words. sort((a,b)=> a-b )
+//console.log(reuilUp)
+
+const resuilDown =  words.sort((a,b)=> a-b)
+const resersed = reuilDown.reserve()
+console.log(resuilDown)
